db033635524dc357d4c242d80cd6273be8f7c60b
DearAJ <aijunyang@sjtu.edu.cn>
2026-01-01 23:30:44 +0800

    feat: node3修改

---
+++ 20260101_ml1m_gexingdu_two_loss_dr.sh
@@ -0,0 +1,69 @@
+python PAAC_main_gexingdu_twoloss_dro.py \
+     --dataset_name ml-1m \
+    --layers_list '[2]' \
+    --cl_rate_list '[0.1]' \
+    --align_reg_list '[0.5]' \
+    --lambada_list '[0.8]' \
+    --gama_list '[0.8]' \
+    --device 0 \
+    --batch_size 2048 \
+    --lr 0.001 \
+    --decay 0.0001 \
+    --emb_size 64 \
+    --num_epoch 1000 \
+    --EarlyStop 10 \
+    --topks '[20]' \
+    --seed 12345 \
+    --tau_list '[0.5]' \
+    --pop_gamma_list '[0.2]' \
+    --inter_rate_list '[1.0, 5.0, 10.0, 0.5]' \
+    --origin_bpr_rate_list '[1.0]' \
+    --dro_rate_list '[0.1, 0.2, 0.5, 0.7, 1.0]'
+
+
+python PAAC_main_gexingdu_twoloss_dro.py \
+    --dataset_name ml-1m \
+    --layers_list '[2]' \
+    --cl_rate_list '[0.1]' \
+    --align_reg_list '[0.5]' \
+    --lambada_list '[0.8]' \
+    --gama_list '[0.8]' \
+    --device 0 \
+    --batch_size 2048 \
+    --lr 0.001 \
+    --decay 0.0001 \
+    --emb_size 64 \
+    --num_epoch 1000 \
+    --EarlyStop 10 \
+    --topks '[20]' \
+    --seed 12345 \
+    --tau_list '[0.5]' \
+    --pop_gamma_list '[0.2]' \
+    --inter_rate_list '[0.0]' \
+    --origin_bpr_rate_list '[1.0]' \
+    --dro_rate_list '[0.0]'
+
+
+
+python PAAC_main_gexingdu_twoloss_dro.py \
+   --dataset_name ml-1m \
+    --layers_list '[2]' \
+    --cl_rate_list '[0.1]' \
+    --align_reg_list '[0.5]' \
+    --lambada_list '[0.8]' \
+    --gama_list '[0.8]' \
+    --device 0 \
+    --batch_size 2048 \
+    --lr 0.001 \
+    --decay 0.0001 \
+    --emb_size 64 \
+    --num_epoch 1000 \
+    --EarlyStop 10 \
+    --topks '[20]' \
+    --seed 12345 \
+    --tau_list '[0.5]' \
+    --pop_gamma_list '[0.2]' \
+    --inter_rate_list '[1.0]' \
+    --origin_bpr_rate_list '[0.0]' \
+    --dro_rate_list '[0.0]'
+
